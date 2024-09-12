34d0940a65c6cac1238a3bcc1375bb1b1f26c218
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-09-12 14:00:23 +0300

    Some fixes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <syncstream>
 #include <vector>
 #include <windows.h>
 
 DWORD WINAPI MyThreadFunction(const LPVOID lpParam) {
+    auto syncStream = std::osyncstream(std::cout);
     const auto threadNum = *static_cast<int *>(lpParam);
     delete static_cast<int *>(lpParam);
-    std::cout << "Thread #" << threadNum << " is running..." << std::endl;
+    syncStream << "Thread #" << threadNum << " is running..." << std::endl;
     Sleep(1000);
-    std::cout << "Thread #" << threadNum << " is terminating..." << std::endl;
-    ExitThread(0);
+    syncStream << "Thread #" << threadNum << " is terminating..." << std::endl;
+    return 0;
 };
 
 int main(const int argc, char *argv[]) {
@@ -21,13 +23,12 @@ int main(const int argc, char *argv[]) {
     std::vector<HANDLE> threads(numThreads);
 
     for (int i = 0; i < numThreads; ++i) {
-        auto threadNum = new int(i + 1);
+        const auto threadNum = new int(i + 1);
         threads[i] = CreateThread(nullptr, 0, MyThreadFunction, threadNum, CREATE_SUSPENDED, nullptr);
 
         if (threads[i] == nullptr) {
             std::cerr << "Error: unable to create thread " << i + 1 << std::endl;
             delete threadNum;
-            return 1;
         }
     }
 
@@ -35,7 +36,12 @@ int main(const int argc, char *argv[]) {
         ResumeThread(thread);
     }
 
+
     WaitForMultipleObjects(numThreads, threads.data(), true, INFINITE);
+
+    for (int i = 0; i < numThreads; ++i) {
+        CloseHandle(threads[i]);
+    }
 
     return 0;
 }
