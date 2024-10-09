36ab96310792dd1dbe397ff35ad7fe8180a52381
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-09 21:38:40 +0300

    Modify file reading and writing

--- lab2/libbmp/libbmp.cpp
+++ lab2/libbmp/libbmp.cpp
@@ -23,8 +23,13 @@ void readBMP(const std::string &filename, int32_t &width, int32_t &height, std::
     pixels.resize(height, std::vector<RGB>(width));
 
     file.seekg(fileHeader.offsetData, std::ios::beg);
+
+    int32_t rowStride = width * sizeof(RGB);
+    int32_t padding = (4 - (rowStride % 4)) % 4;
+
     for (int32_t y = 0; y < height; ++y) {
-        file.read(reinterpret_cast<char *>(pixels[y].data()), width * sizeof(RGB));
+        file.read(reinterpret_cast<char *>(pixels[y].data()), rowStride);
+        file.ignore(padding); // Пропускаем выравнивание
     }
 }
 
@@ -33,10 +38,14 @@ void writeBMP(const std::string &filename, int32_t width, int32_t height,
     BMPFileHeader fileHeader;
     BMPInfoHeader infoHeader;
 
-    fileHeader.fileSize = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + width * height * sizeof(RGB);
+    int32_t rowStride = width * sizeof(RGB);
+    int32_t padding = (4 - (rowStride % 4)) % 4;
+    int32_t paddedRowSize = rowStride + padding;
+
+    fileHeader.fileSize = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + paddedRowSize * height;
     infoHeader.width = width;
     infoHeader.height = height;
-    infoHeader.imageSize = width * height * sizeof(RGB);
+    infoHeader.imageSize = paddedRowSize * height;
 
     std::ofstream file(filename, std::ios::binary);
     if (!file) {
@@ -46,7 +55,9 @@ void writeBMP(const std::string &filename, int32_t width, int32_t height,
 
     file.write(reinterpret_cast<const char *>(&fileHeader), sizeof(fileHeader));
     file.write(reinterpret_cast<const char *>(&infoHeader), sizeof(infoHeader));
+
     for (const auto &row: pixels) {
-        file.write(reinterpret_cast<const char *>(row.data()), width * sizeof(RGB));
+        file.write(reinterpret_cast<const char *>(row.data()), rowStride);
+        file.write("\0\0\0", padding);
     }
 }
