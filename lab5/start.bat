b1a6e7be9ed889272b6c8a04e7764ea0730f99fe
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-11-14 09:47:32 +0300

    Заменить пути на относительные

--- lab5/start.bat
+++ lab5/start.bat
@@ -1,4 +1,4 @@
 @echo off
-start "first" "C:\Users\roman\CLionProjects\parallel-programming\lab5\cmake-build-debug\lab5.exe"
-start "second" "C:\Users\roman\CLionProjects\parallel-programming\lab5\cmake-build-debug\lab5.exe"
+start "first" ".\cmake-build-debug\lab5.exe"
+start "second" ".\cmake-build-debug\lab5.exe"
 exit
