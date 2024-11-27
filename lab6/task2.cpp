cdf7dec77f68d95fc459a79d671e4df510059cac
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-11-27 21:14:14 +0300

    А

--- lab6/task2.cpp
+++ lab6/task2.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     int x = 44;
-#pragma omp parallel for private(x)
+// #pragma omp parallel for private(x)
 // #pragma omp parallel for firstprivate(x) для наглядности можно заменить `=` на `+=`
 // #pragma omp parallel for lastprivate(x)
     for (int i = 0; i <= 10; i++) {
